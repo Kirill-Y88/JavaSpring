5fd609cd2b9f6a655ea59b02a25d16e1a1c1bae9
Kirill <benberov09@mail.ru>
2021-02-18 23:07:43 +0300

    Commit_18.02_HW3

---
+++ src/main/java/ru/geekbrains/spring/mvc/ProductRepo.java
@@ -0,0 +1,74 @@
+package ru.geekbrains.spring.mvc;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ProductRepo implements IProductRepo {
+
+
+    List<Product> products;
+
+    @Override
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public int read(String title){
+        for (Product p: products  ) {
+            if(p.getTitle().equals(title)){
+                return p.getId();
+            }
+        }
+        return 0;
+    }
+    @Override
+    public void write(int id, String title, int cost){
+        products.add(new Product(id, title, cost));
+        update();
+    }
+
+    @Override
+    public void write(Product product){
+        products.add(product);
+        update();
+    }
+
+
+    @Override
+    public void update(){
+        for (Product p:products ) {
+            System.out.printf(" id = %d, title = %s, cost = %d  \n", p.getId(), p.getTitle(), p.getCost());
+        }
+
+    }
+    @Override
+    public void delete(String title) {
+        Product temp = null;
+        for (Product p: products  ) {
+            if(p.getTitle().equals(title)){
+                temp = p;
+                break;
+            }
+        }
+        products.remove(temp);
+        if (temp.equals(null)){
+            System.out.println(" Данный товар отсутствует");
+        }
+        update();
+    }
+
+    @PostConstruct
+    public void init() {
+        products = new ArrayList<>();
+        products.add(new Product(1, "Milk", 15));
+        products.add(new Product(2, "Bread", 55));
+        products.add(new Product(3, "Ham", 100));
+    }
+
+
+}
